9c93110b6871135195f3c5b7930830a1a1148eeb
Jackie2G <jacek.zarychta@smcebi.edu.pl>
2020-11-05 17:31:20 +0100

    naprawa bledow/czyszczenie kodu

--- PracaInzynierska/Controllers/TrainingController.cs
+++ PracaInzynierska/Controllers/TrainingController.cs
@@ -15,14 +15,6 @@ using PracaInzynierska.Models;
 
 namespace PracaInzynierska.Controllers
 {
-    [Serializable]
-    public class Hold
-    {
-        [TempData]
-        public DateTime date { get; set; }
-    }
-
-
     public class TrainingController : Controller
     {
         private readonly ApplicationContext _db;
@@ -30,24 +22,17 @@ namespace PracaInzynierska.Controllers
         public TrainingHistory training { get; set; }
         [BindProperty]
         public Exercises exercise { get; set; }
-        [BindProperty]
-        public DateTime trainingDay { get; set; } 
-        [BindProperty]
-        public string test1 { get; set; }
-        [TempData]
-        public string date { get; set; }
-        //Hold hold = new Hold();
+        //[BindProperty]
+        //public DateTime trainingDay { get; set; } 
 
         public TrainingController(ApplicationContext db)
         {
             _db = db;
         }
 
-        public IActionResult Index(Hold hold)
+        public IActionResult Index()
         {
-            
-            //this.HttpContext.Session.SetString("date", hold.date.ToString());
-            return View(hold);
+            return View();
         }
 
         public IActionResult UpsertExercise(int? id)
@@ -65,55 +50,19 @@ namespace PracaInzynierska.Controllers
             return View(exercise);
         }
 
-        //[HttpPost]
-        ////[ValidateAntiForgeryToken]
-        //public IActionResult SetData(string data)
-        //{
-        //    //Debug.WriteLine(test1);
-        //    //Debug.WriteLine(trainingDay);
-        //    //test1 = Request.Query["test"];
-        //    //var test = form["test"];
-        //    //string testtt = HttpContext.Request.Form["test"];
-        //    //return View();
-        //    //return Json(new { data = test1 });
-        //    //return "dane" + data;
-        //    var time = Convert.ToDateTime(data.Replace("/", ".") + " 00:00:00");
-        //    test1 = data;
-        //    //hold = new Hold();
-        //    //hold.date = Convert.ToDateTime(data.Replace("/", ".") + " 00:00:00");
-        //    training.Date = Convert.ToDateTime(data.Replace("/", ".") + " 00:00:00");
-        //    //this.HttpContext.Session.SetString("date", hold.date.ToString());
-        //    TempData["date"] = data;
-
-        //    return RedirectToAction("Index", new { hold = new Hold() { date = time } });
-        //}
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult UpsertExercise(string data)
         {
-            //var test = Convert.ToDateTime(data.Replace("/", ".") + " 00:00:00");
             if (ModelState.IsValid)
             {
                 if (exercise.ID == 0)
                 {
                     training.Id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-                    Debug.WriteLine(training.Date.ToString());
-                    //training.Date = SetData(data);
-                    if (trainingDay == Convert.ToDateTime("01.01.0001 00:00:00"))
-                    {
-                        training.Date = DateTime.Now;
-                    }
-                    else
-                    {
-                        training.Date = trainingDay;
-                    }
-                    //training.Date = DateTime.Now;
+                    
                     training.ExercisesID = exercise.ID;
+                    training.Date = exercise.trainingHistory.Date;
                     exercise.trainingHistory = training;
-
-                    //var test = Request.Form["datepicker"];
-                    //Debug.Write(test1.ToString());
 
                     _db.ExercisesDb.Add(exercise);
                     _db.TrainingsDb.Add(training);
@@ -125,8 +74,10 @@ namespace PracaInzynierska.Controllers
                     _db.ExercisesDb.Update(exercise);
                     _db.SaveChanges();
                 }
+
                 return RedirectToAction("Index");
             }
+
             return View(exercise);
         }
 
@@ -138,26 +89,22 @@ namespace PracaInzynierska.Controllers
 
             var finalList = list.Where(x => x.trainingHistory.Id.Equals(user)).ToList();
 
-            List <Exercises> lista = new List<Exercises>(finalList);
-
-            lista.ForEach(x => x.trainingHistory.Date.ToString("dd/MM//yyyy"));
-            //return Json(new { data = await _db.ExercisesDb.Where(x => x.trainingHistory.Id.Equals(user)).ToListAsync() });
             return Json(new { data = finalList });
         }
 
-        [HttpGet]
-        public async Task<IActionResult> SetData(string data)
-        {
-            var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var time = data.Replace("/", ".");
+        //[HttpGet]
+        //public async Task<IActionResult> SetData(string data)
+        //{
+        //    var user = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //    var time = data.Replace("/", ".");
     
-            var list = await _db.ExercisesDb.Include(i => i.trainingHistory).ToListAsync();
-            var finalList = list.Where(x => x.trainingHistory.Id.Equals(user)).Where(x => x.trainingHistory.Date.ToString().Contains(time)).ToList();
+        //    var list = await _db.ExercisesDb.Include(i => i.trainingHistory).ToListAsync();
+        //    var finalList = list.Where(x => x.trainingHistory.Id.Equals(user)).Where(x => x.trainingHistory.Date.ToString().Contains(time)).ToList();
 
-            var tescik = await _db.ExercisesDb.ToListAsync();
+        //    var tescik = await _db.ExercisesDb.ToListAsync();
 
-            return Json(new { data = tescik });
-        }
+        //    return Json(new { data = tescik });
+        //}
 
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
@@ -173,12 +120,5 @@ namespace PracaInzynierska.Controllers
 
             return Json(new { success = true, message = "Record deleted" });
         }
-
-        //[HttpPost]
-        //public async Task<IActionResult> SetData()
-        //{
-        //    date = "test1";
-        //    return Json(new { data = date });
-        //}
     }
 }
