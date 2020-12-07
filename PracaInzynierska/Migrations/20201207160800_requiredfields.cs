725eb6c4711df43f37b26b186fc0816a8a54ed52
Jackie2G <jacek.zarychta@smcebi.edu.pl>
2020-12-07 17:11:47 +0100

    dzialajaca wersja bez zauwazalnych bledow

---
+++ PracaInzynierska/Migrations/20201207160800_requiredfields.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PracaInzynierska.Migrations
+{
+    public partial class requiredfields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Exercises",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Exercises",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- PracaInzynierska/Models/Exercises.cs
+++ PracaInzynierska/Models/Exercises.cs
@@ -9,10 +9,14 @@ namespace PracaInzynierska.Models
         //[Key]
         public int ID { get; set; }
         //public int TraningHistoryID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Weight { get; set; }
+        [Required]
         public int Reps { get; set; }
         public bool Done { get; set; }
+        [Required]
         public int Series { get; set; }
         public string Description { get; set; }
         public string CurrentPr { get; set; }
